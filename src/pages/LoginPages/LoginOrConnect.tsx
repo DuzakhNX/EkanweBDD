d022690ffc38b2a6144cf763c67a0e3d51356f25
DuzakhNX <boubacar.boulkassoum@epitech.eu>
2025-05-01 14:04:19 +0200

    HAHA

--- src/pages/LoginPages/LoginOrConnect.tsx
+++ src/pages/LoginPages/LoginOrConnect.tsx
@@ -1,12 +1,28 @@
 import { useNavigate } from "react-router-dom";
 import logo from "../../assets/ekanwe-logo.png";
 import usingLaptop from "../../assets/characterWorkingOnLaptop.png";
+import { useLocation } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 export default function LoginOrConnect() {
   const navigate = useNavigate();
+  const location = useLocation();
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    if (location.state?.error) {
+      setError(location.state.error);
+      window.history.replaceState({}, document.title);
+    }
+  }, [location]);
 
   return (
     <div className="min-h-screen bg-[#1A2C24] relative flex flex-col items-center justify-between p-6">
+      {error && (
+        <div className="bg-red-100 text-red-700 p-3 mb-4 rounded border border-red-400 text-sm">
+          {error}
+        </div>
+      )}
       <div className="w-full text-white text-xs mb-4">
         Une communauté qui offre ensemble une visibilité qui circule. <br />
         Rejoins le mouvement EKANWE.
@@ -17,7 +33,7 @@ export default function LoginOrConnect() {
           Ton contenu a de la valeur. Échange-le contre des offres locales. Inscris-toi gratuitement.
         </p>
         <button className="w-full text-xs rounded-xl border border-[#aec9b6] p-2 mb-6"
-        onClick={() => navigate("/login")}
+          onClick={() => navigate("/login")}
         >
           CONNEXION
         </button>
@@ -40,7 +56,7 @@ export default function LoginOrConnect() {
           bottom: "250px",
           left: "30px",
           zIndex: 0,
-        //   transform: "scaleX(-1)",
+          //   transform: "scaleX(-1)",
         }}
       />
     </div>
